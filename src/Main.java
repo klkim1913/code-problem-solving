9949955924343ea562b6afd3b39c3ed9da92363a
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-09 02:27:29 +0900

    swea: 2072 - 홀수만 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNum = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+
+        for (int i = 0; i < caseNum; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int cnt = st.countTokens();
+            for (int j = 0; j < cnt; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if (n % 2 == 1) {
+                    sum += n;
+                }
+            }
+            System.out.println("#" + (i + 1) + " " + sum);
+            sum = 0;
+        }
     }
 }
