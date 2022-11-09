930dac429d7658fad60966ce3a193085e57cf26e
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-10 02:35:33 +0900

    swea: 1204. 최빈수 구하기

---
+++ src/FindMostFrequentNumber.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class FindMostFrequentNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseCount = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < caseCount; i++) {
+            int caseNum = Integer.parseInt(br.readLine());
+            int[] cntArr = new int[101];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                cntArr[Integer.parseInt(st.nextToken())]++;
+            }
+            int maxCnt = Arrays.stream(cntArr).max().getAsInt();
+            int most = Arrays.stream(cntArr).boxed().collect(Collectors.toList()).lastIndexOf(maxCnt);
+            System.out.println("#" + caseNum + " " + most);
+        }
+    }
+}
