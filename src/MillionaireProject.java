76bac2981bee3390ba7f6902856d3b149a4c2f85
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-11 02:41:34 +0900

    swea: 1859. 백만 장자 프로젝트

---
+++ src/MillionaireProject.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MillionaireProject {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[N];
+            for (int j = 0; j < N; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            long sum = 0L;
+            int max = 0;
+            for (int j = N - 1; j >= 0; j--) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                } else {
+                    sum += max - arr[j];
+                }
+            }
+            System.out.println("#" + (i + 1) + " " + sum);
+        }
+    }
+}
