aed324522afbcdbef14c047782c0af65848eb1f6
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-16 21:13:40 +0900

    bj: 11053. 가장 긴 증가하는 부분 수열

---
+++ src/LongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class LongestIncreasingSubsequence {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] D = new int[N];
+
+        for (int i = 0; i < D.length; i++) {
+            D[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j]) {
+                    D[i] = Math.max(D[i], D[j] + 1);
+                }
+            }
+        }
+
+        int max = Arrays.stream(D).max().getAsInt();
+        System.out.print(max);
+    }
+}
