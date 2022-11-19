152bbb07dab850aaab4cd163fdbb9f97211cbf2c
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-19 14:30:09 +0900

    swea: 1217. [S/W 문제해결 기본] 4일차 - 거듭 제곱

---
+++ src/swea/d3/Dat4Power.java
@@ -0,0 +1,22 @@
+package swea.d3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dat4Power {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int t = 0; t < 10; t++) {
+            int tc = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int base = Integer.parseInt(st.nextToken());
+            int exp = Integer.parseInt(st.nextToken());
+            int result = 1;
+            for (int i = 0; i < exp; i++) {
+                result *= base;
+            }
+            System.out.printf("#%d %d\n", tc, result);
+        }
+    }
+}
