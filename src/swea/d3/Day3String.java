fcac9ce415204c1c5c2695133703113f05a66e02
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-19 14:37:28 +0900

    swea: 1217. [S/W 문제해결 기본] 4일차 - 거듭 제곱 - 수정

--- src/swea/d3/Day3String.java
+++ src/swea/d3/Day3String.java
@@ -2,22 +2,25 @@ package swea.d3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Day3String {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int t = 1; t <= 10; t++) {
+        for (int t = 0; t < 10; t++) {
             int tc = Integer.parseInt(br.readLine());
-            String word = br.readLine();
-            String sentence = br.readLine();
-            sentence = sentence.replace(word, " ");
-            int sum = 0;
-            for (char c : sentence.toCharArray()) {
-                if (c == ' ') {
-                    sum++;
-                }
-            }
-            System.out.printf("#%d %d\n", tc, sum);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int base = Integer.parseInt(st.nextToken());
+            int exp = Integer.parseInt(st.nextToken());
+            int result = power(base, exp);
+            System.out.printf("#%d %d\n", tc, result);
         }
+    }
+
+    static int power(int b, int e) {
+        if (e < 2) {
+            return b;
+        }
+        return b * power(b, e - 1);
     }
 }
