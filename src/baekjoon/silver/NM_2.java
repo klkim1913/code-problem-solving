631e82ca43836755884582a89055482af650c97d
Kyoungryun Kim <krkim1913@gmail.com>
2022-11-23 19:48:39 +0900

    bj: 15650. N과 M (2)

---
+++ src/baekjoon/silver/NM_2.java
@@ -0,0 +1,39 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NM_2 {
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int st, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = st; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+
+}
