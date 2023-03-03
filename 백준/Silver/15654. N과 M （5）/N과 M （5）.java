e2559c61785c000f25834e999f2d252d7758ec0b
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-03 23:57:16 +0900

    [Silver III] Title: N과 M (5), Time: 228 ms, Memory: 30516 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] b;
-    static boolean[] v;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,19 +12,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        b = new int[M];
-        v = new boolean[N];
+        int[] b = new int[M];
+        boolean[] v = new boolean[N];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        perm(0, arr, N, M, sb);
+        perm(0, arr, N, M, sb, b, v);
         System.out.println(sb);
         br.close();
     }
 
-    private static void perm(int cnt, int[] arr, int N, int M, StringBuilder sb) {
+    private static void perm(int cnt, int[] arr, int N, int M, StringBuilder sb, int[] b,
+            boolean[] v) {
         if (cnt == M) {
             for (int n : b) {
                 sb.append(n).append(' ');
@@ -38,7 +37,7 @@ public class Main {
             if (!v[i]) {
                 v[i] = true;
                 b[cnt] = arr[i];
-                perm(cnt + 1, arr, N, M, sb);
+                perm(cnt + 1, arr, N, M, sb, b, v);
                 v[i] = false;
             }
         }
