d09d291417cd08f527417acf935d6b6bb5a98d4f
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-05 01:37:27 +0900

    [Silver III] Title: N과 M (7), Time: 492 ms, Memory: 199704 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[] a;
-    static StringBuilder sb;
     static StringTokenizer st;
+    static StringBuilder sb;
+    static BufferedWriter bw;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -22,13 +27,15 @@ public class Main {
         }
         Arrays.sort(a);
         perm(0, "");
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
         br.close();
+
     }
 
-    private static void perm(int cnt, String str) {
+    private static void perm(int cnt, String str) throws Exception {
         if (cnt == M) {
-            sb.append(str).append("\n");
+            bw.write(str + "\n");
             return;
         }
         for (int i = 0; i < N; i++) {
