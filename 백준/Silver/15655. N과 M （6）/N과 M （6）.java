18a932f1e8594355b287748b84386b25e4f046ce
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-04 21:44:45 +0900

    [Silver III] Title: N과 M (6), Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -4,36 +4,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] a, b;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] a = new int[N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        a = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
-        int[] b = new int[M];
         Arrays.sort(a);
-        comb(0, 0, N, M, a, b, sb);
-        System.out.println(sb);
+        comb(0, 0, "");
+        System.out.print(sb);
         br.close();
     }
 
-    private static void comb(int cnt, int start, int N, int M, int[] a, int[] b, StringBuilder sb) {
+    private static void comb(int cnt, int start, String str) {
         if (cnt == M) {
-            for (int n : b) {
-                sb.append(n).append(' ');
-            }
-            sb.append('\n');
+            sb.append(str).append('\n');
             return;
         }
         for (int i = start; i < N; i++) {
-            b[cnt] = a[i];
-            comb(cnt + 1, i + 1, N, M, a, b, sb);
+            comb(cnt + 1, i + 1, str + a[i] + " ");
         }
     }
 
