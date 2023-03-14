cd7172f0ef50eb5c90edf39b77727f7b90346f93
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-14 17:28:20 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 100 ms, Memory: 12372 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,54 +1,73 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] g;
-	static int[] p;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int[] parent;
+	static int N, M, cnt;
+	
+	public static void main(String[] args) throws IOException {
+		
+		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		makeSet();
+		M = Integer.parseInt(st.nextToken());
+		make();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken()) - 1;
-			int to = Integer.parseInt(st.nextToken()) - 1;
-			union(from, to);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(union(a, b)) {
+				if(++cnt == N - 1) {
+					System.out.println(1);
+					return;
+				}
+			}
 		}
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			if (p[i] == i)
-				cnt++;
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 1; i <= N; i++) {
+			set.add(find(i));
 		}
-		System.out.println(cnt);
-		br.close();
+		System.out.println(set.size());
+		
 	}
-
-	private static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if (aRoot == bRoot)
-			return false;
-		p[bRoot] = aRoot;
+	
+	static boolean union(int a, int b) {
+		
+		a = find(a);
+		b = find(b);
+		
+		if(a == b) return false;
+		
+		if(a < b) parent[b] = a;
+		else parent[a] = b;
 		return true;
+		
 	}
-
-	private static int find(int a) {
-		if (p[a] == a)
-			return a;
-		return p[a] = find(p[a]);
+	
+	static int find(int a) {
+		
+		if(parent[a] == a) return a;
+		
+		return parent[a] = find(parent[a]);
+		
 	}
-
-	private static void makeSet() {
-		p = new int[N];
-		for (int i = 0; i < N; i++) {
-			p[i] = i;
+	
+	static void make() {
+		
+		parent = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			
+			parent[i] = i;
+			
 		}
+		
 	}
-
+	
 }
