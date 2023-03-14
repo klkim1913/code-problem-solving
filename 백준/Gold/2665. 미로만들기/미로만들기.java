65d63a02ce0eda46966782abd22acd1e4894bcd8
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-14 13:13:06 +0900

    [Gold IV] Title: 미로만들기, Time: 80 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -18,38 +19,33 @@ public class Main {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = s.charAt(j) - '0';
-				arr[i][j] = (arr[i][j] == 1) ? 0 : 1;
 			}
 		}
-		int[][] dist = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				dist[i][j] = N * N;
-			}
-		}
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
-		dist[0][0] = 0;
-		pq.offer(new int[] { 0, 0, 0 });
-		while (!pq.isEmpty()) {
-			int[] cur = pq.poll();
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] { 0, 0, 0 });
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
 			int i = cur[0];
 			int j = cur[1];
 			int black = cur[2];
 			if (v[i][j])
 				continue;
 			v[i][j] = true;
-			if (i == N - 1 && j == N - 1)
+			if (i == N - 1 && j == N - 1) {
+				System.out.println(black);
 				break;
+			}
 			for (int d = 0; d < 4; d++) {
 				int ni = i + di[d];
 				int nj = j + dj[d];
-				if (0 <= ni && ni < N && 0 <= nj && nj < N && !v[ni][nj] && dist[ni][nj] > black + arr[ni][nj]) {
-					dist[ni][nj] = black + arr[ni][nj];
-					pq.offer(new int[] { ni, nj, dist[ni][nj] });
+				if (0 <= ni && ni < N && 0 <= nj && nj < N && !v[ni][nj]) {
+					if (arr[ni][nj] == 0)
+						q.offer(new int[] { ni, nj, black + 1 });
+					else
+						q.push(new int[] { ni, nj, black });
 				}
 			}
 		}
-		System.out.println(dist[N - 1][N - 1]);
 		br.close();
 	}
 
