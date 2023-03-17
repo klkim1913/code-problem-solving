748da8a63c108bdbdb8cd0c988629898693b8639
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-17 23:25:40 +0900

    [Bronze IV] Title: 10부제, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int one = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            if (Integer.parseInt(st.nextToken()) % 10 == one)
+                sum++;
+        }
+        System.out.print(sum);
+        br.close();
+    }
+
+}
