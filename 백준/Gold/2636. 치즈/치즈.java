dcc8f976837b4f21388efe9827962f2dc63c4260
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-03-31 14:16:12 +0900

    [Gold IV] Title: 치즈, Time: 96 ms, Memory: 12620 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -10,7 +10,12 @@ public class Main {
 	static final int cheeze = 1;
 	// 현재 시간에 먹은 치즈 체크
 	static final int ate = 2;
+	// 남은 치즈 수
 	static int left;
+
+	// dfs
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,15 +27,20 @@ public class Main {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < C; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				// 치즈의 개수 체크
 				if (map[i][j] == cheeze)
 					left++;
 			}
 		}
+		// 걸리는 시간
 		int hour = 0;
+		// 이전 시간에 남았던 치즈
 		int prevCheeze = left;
 		// 가장자리 치즈 탐색
 		while (left != 0) {
-			bfs();
+//			bfs();
+			dfs(0, 0, new boolean[R][C]);
+			clear();
 			if (left != 0)
 				prevCheeze = left;
 			hour++;
@@ -40,33 +50,7 @@ public class Main {
 		br.close();
 	}
 
-	private static void bfs() {
-		int[] dr = { -1, 0, 1, 0 };
-		int[] dc = { 0, 1, 0, -1 };
-		boolean[][] v = new boolean[R][C];
-		ArrayDeque<int[]> q = new ArrayDeque<>();
-		v[0][0] = true;
-		q.offer(new int[] { 0, 0 });
-		while (!q.isEmpty()) {
-			int[] rc = q.poll();
-			int r = rc[0];
-			int c = rc[1];
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				if (0 <= nr && nr < R && 0 <= nc && nc < C && !v[nr][nc] && map[nr][nc] != ate) {
-					if (map[nr][nc] == cheeze) {
-						left--;
-						map[nr][nc] = ate;
-						if (left == 0)
-							break;
-					} else {
-						v[nr][nc] = true;
-						q.offer(new int[] { nr, nc });
-					}
-				}
-			}
-		}
+	private static void clear() {
 		// 먹은 치즈 제거
 		if (left != 0) {
 			for (int i = 0; i < R; i++) {
@@ -76,6 +60,67 @@ public class Main {
 				}
 			}
 		}
+	}
+
+	private static void dfs(int r, int c, boolean[][] v) {
+		// 남은 치즈가 없을경우 종료
+		if (left == 0)
+			return;
+		v[r][c] = true;
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			// 이번시간에 먹은 치즈는 방문하면 안됨.
+			if (0 <= nr && nr < R && 0 <= nc && nc < C && !v[nr][nc] && map[nr][nc] != ate) {
+				if (map[nr][nc] == cheeze) {
+					left--;
+					// 다르게 임시 표시를 해줘야 재방문하거나 치즈를 더 먹는 일이 없다.
+					map[nr][nc] = ate;
+					// 남은 치즈가 없을 경우 즉시 종료
+					if (left == 0)
+						return;
+					// 빈 공간일 경우
+				} else {
+					dfs(nr, nc, v);
+				}
+			}
+		}
+	}
+
+	private static void bfs() {
+		int[] dr = { -1, 0, 1, 0 };
+		int[] dc = { 0, 1, 0, -1 };
+
+		boolean[][] v = new boolean[R][C];
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+
+		v[0][0] = true;
+		q.offer(new int[] { 0, 0 });
+		while (!q.isEmpty()) {
+			int[] rc = q.poll();
+			int r = rc[0];
+			int c = rc[1];
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				// 이번시간에 먹은 치즈는 방문하면 안됨.
+				if (0 <= nr && nr < R && 0 <= nc && nc < C && !v[nr][nc] && map[nr][nc] != ate) {
+					if (map[nr][nc] == cheeze) {
+						left--;
+						// 다르게 임시 표시를 해줘야 재방문하거나 치즈를 더 먹는 일이 없다.
+						map[nr][nc] = ate;
+						// 남은 치즈가 없을경우 종료
+						if (left == 0)
+							break;
+						// 빈 공간일 경우
+					} else {
+						v[nr][nc] = true;
+						q.offer(new int[] { nr, nc });
+					}
+				}
+			}
+		}
+
 	}
 
 }
