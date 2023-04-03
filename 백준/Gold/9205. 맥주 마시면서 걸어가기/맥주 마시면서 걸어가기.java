a2f7897f4a35ea28e8cb580f3bd5984020af9d83
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-04-03 16:42:12 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 116 ms, Memory: 13552 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,52 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
-
-import org.omg.PortableInterceptor.DISCARDING;
 
 // 한박스 맥주 20개
 // 50미터에 한병씩 마셔야함
 // 편의점에 들릴 시 빈 병은 버리고 새 맥주 살 수 있음(최대 20병 보유 가능)
 public class Main {
+	static int n;
+	static boolean[][] distance;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		final int INF = Integer.MAX_VALUE / 2;
 		for (int tc = 0; tc < T; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int[] home = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
+			n = Integer.parseInt(br.readLine());
 			int[][] arr = new int[n + 2][2];
-			arr[0] = home;
-			for (int i = 1; i < n + 1; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < n + 2; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				arr[i] = new int[] { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
 			}
-			st = new StringTokenizer(br.readLine(), " ");
-			int[] dest = new int[] { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
-			arr[n + 1] = dest;
-			int[][] distance = new int[n + 2][n + 2];
+			distance = new boolean[n + 2][n + 2];
 			for (int i = 0; i < n + 2; i++) {
 				for (int j = 0; j < n + 2; j++) {
 					if (canMove(arr[i], arr[j]))
-						distance[i][j] = 1;
-					else
-						distance[i][j] = INF;
+						distance[i][j] = true;
 				}
 			}
-			for (int k = 0; k < n + 2; k++) {
-				for (int i = 0; i < n + 2; i++) {
-					for (int j = 0; j < n + 2; j++) {
-						distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
-					}
-				}
-			}
-			sb.append(distance[0][n + 1] == INF ? "sad" : "happy").append('\n');
+
+			sb.append(bfs()).append('\n');
 		}
 		System.out.print(sb);
 		br.close();
+	}
+
+	private static String bfs() {
+		boolean[][] v = new boolean[n + 2][n + 2];
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		v[0][0] = true;
+		q.offer(0);
+		while (!q.isEmpty()) {
+			int i = q.poll();
+			if (i == n + 1)
+				return "happy";
+			for (int j = 1; j < n + 2; j++) {
+				if (distance[i][j] && !v[i][j]) {
+					v[i][j] = true;
+					q.offer(j);
+				}
+			}
+		}
+		return "sad";
 	}
 
 	private static boolean canMove(int[] dot1, int[] dot2) {
