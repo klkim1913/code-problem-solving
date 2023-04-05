c92d84ebe6cd40eba7d2a093ea10411e980217f4
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-04-05 14:14:43 +0900

    [Gold IV] Title: 회전 초밥, Time: 492 ms, Memory: 168808 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -13,11 +13,12 @@ public class Main {
 		int d = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
-		int[] belt = new int[N];
-//		ArrayDeque<Integer> q = new ArrayDeque<>();
+		int[] belt = new int[N + k - 1];
 		for (int i = 0; i < N; i++) {
 			belt[i] = Integer.parseInt(br.readLine());
-//			q.offer(Integer.parseInt(br.readLine()));
+		}
+		for (int i = N; i < N + k - 1; i++) {
+			belt[i] = belt[i - N];
 		}
 		// 현재 집합에서 인덱스 초밥이 몇개 들어있냐
 		int[] dv = new int[d + 1];
@@ -25,36 +26,28 @@ public class Main {
 		// 뒤에꺼 넣을때도 넣었을때 1이면 가지수 추가하고 2 이상이면 안추가하고.
 		// 쿠폰번호 추가했을때도 1이면 가지수 추가하고 2 이상이면 안추가하고.
 		// max값이 k+1이 나오면 바로 종료
-		int kind = 0, max = 0;
-		int left = 0;
-		int right = k - 1;
-		dv[belt[left]]++;
-		kind++;
-		if (dv[belt[right]] == 0)
-			kind++;
-		dv[belt[right++]]++;
-		for (int i = 1; i < k - 1; i++) {
+		int kind = 0;
+		for (int i = 0; i < k; i++) {
 			// 그 음식이 현재 들어있지 않다면 종류가 추가
-			if (dv[belt[i]] == 0)
+			if (dv[belt[i]]++ == 0)
 				kind++;
-			dv[belt[i]]++;
-			if (max < kind)
-				max = kind;
 		}
-		while (left < N) {
+		int max = 0;
+		if (max < kind)
+			max = kind;
+		int left = 0;
+		int right = k;
+		while (right < N + k - 2) {
 			if (max == k + 1)
 				break;
 			// 앞에꺼 뺐는데 0이되면 1종류 빼기
-			if (--dv[belt[left % N]] == 0)
+			if (--dv[belt[left]] == 0)
 				kind--;
 			// 뒤에꺼 추가했는데 처음이면 종류 추가
-			if (++dv[belt[right % N]] == 1)
+			if (++dv[belt[right]] == 1)
 				kind++;
 			// 쿠폰 처음 추가이면 종류 추가
-			if (++dv[c] == 1)
-				kind++;
-			if (max < kind)
-				max = kind;
+			max = Math.max(max, kind + (dv[c] == 0 ? 1 : 0));
 			right++;
 			left++;
 		}
