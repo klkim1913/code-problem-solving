fbdc5cac6e4c5e42e720855c7512d059cc27d27b
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-04-05 10:08:28 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,064 ms, Memory: 91,556 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	static int N, M, cnt;
-	static int[][] g;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,19 +14,20 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
-			g = new int[N + 1][N + 1]; // 자신보다 키가 큰 관계를 표현
+			int[][] g = new int[N + 1][N + 1]; // 자신보다 키가 큰 관계를 표현
+			int[][] rg = new int[N + 1][N + 1]; // 자신보다 키가 작은 관계를 표현
 
 			for (int m = 0; m < M; m++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				g[a][b] = 1; // 유향
+				g[a][b] = rg[b][a] = 1;
 			}
 			int ans = 0;
 			for (int k = 1; k <= N; k++) {
 				cnt = 0;
-				gtDFS(k, new boolean[N + 1]);
-				ltDFS(k, new boolean[N + 1]);
+				dfs(k, g, new boolean[N + 1]);
+				dfs(k, rg, new boolean[N + 1]);
 				if (cnt == N - 1)
 					ans++;
 			}
@@ -37,24 +37,14 @@ public class Solution {
 		br.close();
 	}
 
-	static void gtDFS(int cur, boolean[] visited) {
-		// cur정점 기준으로 자신보다 큰 정점 탐색
+	static void dfs(int cur, int[][] g, boolean[] visited) {
+		// g: 자신보다 큰관계 인접행렬이면 자신보다 큰 정접 탐색
+		// g: 자신보다 작은관계 인접행렬이면 자신보다 큰 정접 탐색
 		visited[cur] = true;
 		for (int i = 1; i <= N; i++) {
 			if (g[cur][i] == 1 && !visited[i]) {
 				cnt++;
-				gtDFS(i, visited);
-			}
-		}
-	}
-
-	static void ltDFS(int cur, boolean[] visited) {
-		// cur정점 기준으로 자신보다 큰 정점 탐색
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (g[i][cur] == 1 && !visited[i]) {
-				cnt++;
-				ltDFS(i, visited);
+				dfs(i, g, visited);
 			}
 		}
 	}
