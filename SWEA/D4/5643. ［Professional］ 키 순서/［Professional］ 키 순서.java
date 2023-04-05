2d2e70836a48097ee871c5e3d985c7f3c3398e93
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-04-05 10:44:09 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,064 ms, Memory: 91,556 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -13,27 +13,12 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int M = Integer.parseInt(br.readLine());
-			ArrayList<ArrayList<Integer>> g = new ArrayList<>();
-			for (int i = 0; i <= N; i++) {
-				g.add(new ArrayList<>());
-			}
+			boolean connected[][] = new boolean[N + 1][N + 1];
 			for (int i = 0; i < M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				g.get(from).add(to);
-			}
-			boolean connected[][];
-			connected = new boolean[N + 1][N + 1];
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					if (i != j) {
-						for (int n : g.get(i)) {
-							if (n == j)
-								connected[i][j] = true;
-						}
-					}
-				}
+				connected[from][to] = true;
 			}
 			for (int k = 1; k <= N; k++) {
 				for (int i = 1; i <= N; i++) {
