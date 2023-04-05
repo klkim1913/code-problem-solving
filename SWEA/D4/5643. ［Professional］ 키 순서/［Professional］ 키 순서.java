ce045767d4da945bac041768022bc15b28d898de
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-04-05 10:35:03 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,064 ms, Memory: 91,556 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,52 +1,68 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, M, cnt;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-
 		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			M = Integer.parseInt(br.readLine());
-			int[][] g = new int[N + 1][N + 1]; // 자신보다 키가 큰 관계를 표현
-			int[][] rg = new int[N + 1][N + 1]; // 자신보다 키가 작은 관계를 표현
-
-			for (int m = 0; m < M; m++) {
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+			ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+			for (int i = 0; i <= N; i++) {
+				g.add(new ArrayList<>());
+			}
+			for (int i = 0; i < M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				g[a][b] = rg[b][a] = 1;
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				g.get(from).add(to);
 			}
-			int ans = 0;
+			boolean connected[][];
+			connected = new boolean[N + 1][N + 1];
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (i != j) {
+						for (int n : g.get(i)) {
+							if (n == j)
+								connected[i][j] = true;
+						}
+					}
+				}
+			}
 			for (int k = 1; k <= N; k++) {
-				cnt = 0;
-				dfs(k, g, new boolean[N + 1]);
-				dfs(k, rg, new boolean[N + 1]);
-				if (cnt == N - 1)
-					ans++;
+				for (int i = 1; i <= N; i++) {
+					for (int j = 1; j <= N; j++) {
+						if (connected[i][k] && connected[k][j]) {
+							connected[i][j] = true;
+						}
+					}
+				}
 			}
-			sb.append("#" + tc + " " + ans + "\n");
+			int sum = 0;
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (i == j) {
+						if (i == N && j == N) {
+							sum++;
+						}
+						continue;
+					}
+					if (!connected[i][j] && !connected[j][i])
+						break;
+					if (j == N)
+						sum++;
+				}
+			}
+			sb.append("#" + tc + " " + sum + "\n");
 		}
 		System.out.print(sb);
 		br.close();
-	}
-
-	static void dfs(int cur, int[][] g, boolean[] visited) {
-		// g: 자신보다 큰관계 인접행렬이면 자신보다 큰 정접 탐색
-		// g: 자신보다 작은관계 인접행렬이면 자신보다 큰 정접 탐색
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (g[cur][i] == 1 && !visited[i]) {
-				cnt++;
-				dfs(i, g, visited);
-			}
-		}
 	}
 
 }
