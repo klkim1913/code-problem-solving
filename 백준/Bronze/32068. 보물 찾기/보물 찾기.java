60380c0be90ee52f28245d2a5066efde98c0f466
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2024-07-18 11:41:30 +0900

    [Bronze III] Title: 보물 찾기, Time: 516 ms, Memory: 22536 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.java
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.java
@@ -7,26 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int L = Integer.parseInt(st.nextToken());
             int R = Integer.parseInt(st.nextToken());
             int S = Integer.parseInt(st.nextToken());
-            int cnt = 1;  // 첫번째에 발견은 불가능
-            boolean buho = true;
-            while (S != L && S != R) {
-                if (buho) {
-                    S += cnt;
-                } else {
-                    S -= cnt;
-                }
-                buho = !buho;
-                cnt++;
+            if (Math.abs(S - R) <= Math.abs(S - L)) {
+                System.out.println(Math.abs(S - R) * 2);
+            } else {
+                System.out.println(Math.abs(S - L) * 2 + 1);
             }
-            sb.append(cnt).append("\n");
         }
-        System.out.print(sb);
         br.close();
     }
 }
